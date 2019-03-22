5cc6eb646b2188fe7a2620a547dc98b2d0743aa8
Jonathan Jackson <jack0355@algonquinlive.com>
2019-03-22 14:52:55 -0400

    Deleted networked aframe

--- app.js
+++ app.js
@@ -1,80 +1,26 @@
-// Load required modules
-var http    = require("http");              // http server core module
-var express = require("express");           // web framework external module
-var serveStatic = require('serve-static');  // serve static files
-var socketIo = require("socket.io");        // web socket external module
-var easyrtc = require("easyrtc");               // EasyRTC external module
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketIO = require('socket.io')(server);
 
-// Set process name
-process.title = "node-easyrtc";
+const LISTEN_PORT = 8080;
 
-// Get port or default to 8080
-var port = process.env.PORT || 8080;
+//middleware
+app.use(express.static(__dirname + '/public'));
 
-// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
-var app = express();
-app.use(serveStatic('public/', {'index': ['index.html']}));
+//set a route
+app.get('/', function(req, res){
+    res.sendFile(__dirname + 'public/index.html');
+});
 
-// Start Express http server
-var webServer = http.createServer(app);
+socketIO.on('connection', function(socket){
+    console.log(socket.id + " has connected!");
 
-// Start Socket.io so it attaches itself to Express server
-var socketServer = socketIo.listen(webServer, {"log level":1});
-
-var myIceServers = [
-  {"url":"stun:stun.l.google.com:19302"},
-  {"url":"stun:stun1.l.google.com:19302"},
-  {"url":"stun:stun2.l.google.com:19302"},
-  {"url":"stun:stun3.l.google.com:19302"}
-  // {
-  //   "url":"turn:[ADDRESS]:[PORT]",
-  //   "username":"[USERNAME]",
-  //   "credential":"[CREDENTIAL]"
-  // },
-  // {
-  //   "url":"turn:[ADDRESS]:[PORT][?transport=tcp]",
-  //   "username":"[USERNAME]",
-  //   "credential":"[CREDENTIAL]"
-  // }
-];
-easyrtc.setOption("appIceServers", myIceServers);
-easyrtc.setOption("logLevel", "debug");
-easyrtc.setOption("demosEnable", false);
-
-// Overriding the default easyrtcAuth listener, only so we can directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
-        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
-            callback(err, connectionObj);
-            return;
-        }
-
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
-
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
-
-        callback(err, connectionObj);
+    socket.on('disconnect', function(data){
+        console.log(socket.id + " has disconnected");
     });
 });
 
-// To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
-    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
-});
-
-// Start EasyRTC server
-var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
-    console.log("Initiated");
-
-    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
-        console.log("roomCreate fired! Trying to create: " + roomName);
-
-        appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
-    });
-});
-
-//listen on port
-webServer.listen(port, function () {
-  console.log('listening on http://localhost:' + port);
-});
+server.listen(LISTEN_PORT);
+console.log('listening to port:' + LISTEN_PORT);
